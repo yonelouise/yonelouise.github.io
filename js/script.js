4006ffd48829f436c9f0d193d10d8f817d9b8bed
Yone Louise <yone_louise@hotmail.com>
2025-03-27 22:36:25 +0100

    adicionando mais css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll(".toggle-btn").forEach(function(btn){btn.addEventListener("click", function () {
+  let text = document.querySelector(".text");
+  
+  if (text.classList.contains("expanded")) {
+    text.classList.remove("expanded");
+    text.style.display = "-webkit-box"; // Retorna ao estado original
+    this.textContent = "Ler mais";
+  } else {
+    text.classList.add("expanded");
+    text.style.display = "block"; // Exibe o texto completo
+    this.textContent = "Ler menos";
+  }
+})});
