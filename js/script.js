30fa3039f7969ddfc12552854dbbe2bd0539410c
Yone Louise <yone_louise@hotmail.com>
2025-03-29 17:53:14 +0100

    finalizando css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-document.querySelectorAll(".toggle-btn").forEach(function(btn){btn.addEventListener("click", function () {
-  let text = document.querySelector(".text");
+// document.querySelectorAll(".toggle-btn").forEach(function(btn){btn.addEventListener("click", function () {
+//   let text = document.querySelector(".text");
   
-  if (text.classList.contains("expanded")) {
-    text.classList.remove("expanded");
-    text.style.display = "-webkit-box"; // Retorna ao estado original
-    this.textContent = "Ler mais";
-  } else {
-    text.classList.add("expanded");
-    text.style.display = "block"; // Exibe o texto completo
-    this.textContent = "Ler menos";
-  }
-})});
+//   if (text.classList.contains("expanded")) {
+//     text.classList.remove("expanded");
+//     text.style.display = "-webkit-box"; // Retorna ao estado original
+//     this.textContent = "Ler mais";
+//   } else {
+//     text.classList.add("expanded");
+//     text.style.display = "block"; // Exibe o texto completo
+//     this.textContent = "Ler menos";
+//   }
+// })});
+
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+menuHamburguer.addEventListener('cl')
